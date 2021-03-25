210b26d5a8333a5289dd7bbd3b19d3c994681550
Mottaaaa <pedro10mota@msn.com>
2021-03-25 19:38:30 +0000

    bodega dos ngrams

--- preprocessing/index.js
+++ preprocessing/index.js
@@ -3,7 +3,7 @@ let stemming = require('./stemming');
 let stopwords = require('./stopwords');
 let tokenization = require('./tokenization');
 
-module.exports = async (str, n) => {
+module.exports = async (original, n) => {
     original = stopwords(original);
     let cleaned = clean(original);
     let stemmed = stemming(cleaned);
