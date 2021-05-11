e92e4a077fa59abc08fd17669374ad7bfcfcff67
Mottaaaa <pedro10mota@msn.com>
2021-05-11 19:01:08 +0100

    Lab 4 Part2 to be continued

--- features/bagOfWords.js
+++ features/bagOfWords.js
@@ -8,8 +8,16 @@ let addUniqueTerms = (arr1, arr2) => {
     return arr1;
 };
 
-let binaryVector = () => {
-
+let binaryVector = (bow, terms) => {
+    let arr = bow;
+    for (let i = 0; i < bow.length; i++) {
+        if (terms.some(t => t.name === bow[i].name)) {
+            arr[i].binary = 1;
+        } else {
+            arr[i].binary = 0;
+        }
+    }
+    return arr;
 };
 
 let compareTerms = (term1, term2) => {
@@ -21,4 +29,75 @@ let compareTerms = (term1, term2) => {
     return sorted1.join('') === sorted2.join('');
 }
 
+let numberOfOccurrencesVector = (bow, terms) => {
+    let arr = bow;
+    for (let i = 0; i < bow.length; i++) {
+        let count = terms.filter(t => t.name === bow[i].name).length;
+        arr[i].occurences = count;
+    }
+    return arr;
+}
+
+let tfVector = (bow, terms) => {
+    let arr = bow;
+    for (let i = 0; i < bow.length; i++) {
+        let count = terms.filter(t => t.name === bow[i].name).length;
+        let total = terms.length;
+        let tf = Math.round(((count / total) + Number.EPSILON) * 100) / 100;
+        arr[i].tf = tf;
+    }
+    return arr;
+}
+
+
+let idfVector = (bow, docBoWs) => {
+    let arr = bow;
+    let n = docBoWs.length;
+    for (let i = 0; i < bow.length; i++) {
+        let term = bow[i];
+        let filteredDocBoWs = docBoWs.filter(docBoW => docBoW.some(t => t.name === term.name && t.occurences > 0));
+
+        let dt = filteredDocBoWs.length;
+        let idf = Math.round(((Math.log10(n / dt)) + Number.EPSILON) * 100) / 100;
+
+        docBoWs.map(docBow => docBow.map(t => {
+            if (t.name === term.name) {
+                t.idf = idf;
+            }
+            return t;
+        }));
+
+        arr[i].idf = idf;
+    }
+    return arr;
+}
+
+let tfidfVector = (bow, docBoWs) => {
+
+    docBoWs.map(docBow => docBow.map(term => {
+        term.tfidf = Math.round((term.tf * term.idf + Number.EPSILON) * 100) / 100;
+        return term;
+    }));
+
+    return bow;
+};
+
+let sumVector = (terms) => {
+    let term = {};
+    term.name = terms[0].name;
+    term.binary = terms.map(term => term.binary).reduce((acc, curr) => acc * curr);
+    term.occurences = terms.map(term => term.occurences).reduce((acc, curr) => acc + curr);
+    term.tf = Math.round(terms.map(term => term.tf).reduce((acc, curr) => acc + curr) * 100)/100;
+    term.idf = terms[0].idf;
+    term.tfidf = Math.round((term.tf * term.idf) * 100) / 100;
+    return term;
+};
+
+
 module.exports.addUniqueTerms = addUniqueTerms;
+module.exports.binaryVector = binaryVector;
+module.exports.numberOfOccurrencesVector = numberOfOccurrencesVector;
+module.exports.tfVector = tfVector;
+module.exports.idfVector = idfVector;
+module.exports.tfidfVector = tfidfVector;
+module.exports.sumVector = sumVector;
--- preprocessing/train.js
+++ preprocessing/train.js
@@ -9,18 +9,24 @@ let process = async () => {
     let classes = {
     };
 
-    let happy = result.filter(e => e.label === 'happy').map(e => e.description);
-    let nHappy = result.filter(e => e.label === 'not happy').map(e => e.description);
+    let happy = result.filter(e => e.label === 'happy');
+    let nHappy = result.filter(e => e.label === 'not happy');
 
     classes['happy'] = {
         elements: happy.map(e => {
-            return { original: e };
+            return {
+                docID: e.id,
+                original: e.description
+            };
         })
     };
 
     classes['notHappy'] = {
         elements: nHappy.map(e => {
-            return { original: e };
+            return {
+                docID: e.id,
+                original: e.description
+            };
         })
     };
 
@@ -41,30 +47,80 @@ let process = async () => {
                 element['characterCount'] = characterCount;
                 element['n' + i] = n.tokenized;
 
-                classes[prop]['n' + i + 'Terms'] = bagOfWords.addUniqueTerms(classes[prop]['n' + i + 'Terms'], JSON.parse(tokenized));
+                classes[prop]['n' + i + 'Terms'] = bagOfWords.addUniqueTerms(classes[prop]['n' + i + 'Terms'], JSON.parse(n.tokenized));
 
                 classes[prop].elements[index] = element;
             }
         }
     }
 
-    for (let index = 0; index < classes[prop].elements.length; index++) {
-        for (let i = 1; i < 3; i++) {
-            let docTerms = mapTerms(n.tokenized);
-
-            // Calculos
-            // Cada documento vai ter todas as palavras que existem no BoW e os respectivos calculos
-            // No BoW é fazer a soma dos calculos dos documentos
-        }
-    }
+    classes = calculate(classes);
+    
 
     fs.writeFileSync(`${__dirname}\\training-process.txt`, JSON.stringify(classes, null, 4));
 };
 
-let mapTerms = (tokenized) => {
-    let parsed = JSON.parse(tokenized);
-    return parsed.map(arr => {
+let calculate = (classes) => {
+    for (let prop in classes) {
+
+        let results = {
+            n1: [],
+            n2: []
+        };
+        let bows = {
+            n1: [],
+            n2: []
+        };
+
+        for (let index = 0; index < classes[prop].elements.length; index++) {
+            for (let i = 1; i < 3; i++) {
+
+                let element = classes[prop].elements[index];
+                let nTerms = classes[prop]['n' + i + 'Terms'];
+
+                // Mapeamento
+                let docTerms = mapTerms(JSON.parse(element['n' + i]), element.docID);
+                bows['n' + i] = mapTerms(nTerms, 0);
+
+                // Calculos
+                let docBoW = bows['n' + i].map(e => {
+                    let elem = Object.assign({}, e);
+                    elem.docID = element.docID;
+                    return elem;
+                });
+
+                docBoW = bagOfWords.binaryVector(docBoW, docTerms);
+                docBoW = bagOfWords.numberOfOccurrencesVector(docBoW, docTerms);
+                docBoW = bagOfWords.tfVector(docBoW, docTerms);
+
+                results['n' + i].push(docBoW);
+            }
+        }
+
+        for (let i = 1; i < 3; i++) {
+            let docBoWs = results['n' + i];
+            let bow = bows['n' + i];
+
+            bow = bagOfWords.idfVector(bow, docBoWs);
+            bow = bagOfWords.tfidfVector(bow, docBoWs);
+
+            bows['n' + i] = bow.map(term => {
+                let filtered = docBoWs.map(docBoW => docBoW.find(t => t.name === term.name));
+                term = bagOfWords.sumVector(filtered);
+                return term;
+            });
+        }
+
+        classes[prop].bows = bows;
+    }
+
+    return classes;
+}
+
+let mapTerms = (tokenized, docID) => {
+    return tokenized.map(arr => {
         return {
+            docID,
             name: arr.sort().join(' '),
             binary: 0,
             occurences: 0,
